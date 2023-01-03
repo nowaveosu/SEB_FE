aba447c2f649f0b317f82c4ac64f4694a9c7cba3
nowaveosu <nowaveosu@gmail.com>
2023-01-03 17:03:58 +0900

    1.3

---
+++ test_js/scope.js
@@ -0,0 +1,9 @@
+function greetSomeone(firstName) {
+    var time = 'night';
+    if(time === 'night') {
+        var greeting = 'Good Night';
+    }
+
+    return greeting + ' ' + firstName;
+}
+greetSomeone('Steve');
