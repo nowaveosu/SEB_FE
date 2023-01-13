3c8bcc0c79b27b97133a917dd95efb3557728c53
nowaveosu <nowaveosu@gmail.com>
2023-01-13 17:28:19 +0900

    1.13

--- test_js/test.js
+++ test_js/test.js
@@ -1,1 +1,12 @@
-arr = [0,1,2]
+let counter1 = {
+    value: 0,
+    increase: function(){
+        this.value++
+    },
+    decrease: function(){
+        this.value--
+    },
+    getValue: function(){
+        return this.value
+    }
+}
